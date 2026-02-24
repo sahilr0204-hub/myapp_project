3c70a36abbfd236c70cad36f3deda5528ff2b394
sahilr0204-hub <sahil.r0204@gmail.com>
2026-02-24 16:54:00 +0530

    version2

--- myapp.py
+++ myapp.py
@@ -1,3 +1,9 @@
+#version1
 num1=int(input("enter first no: "))
 num2=int(input("enter second no: "))
 print("sum of two numbers is ", num1 + num2)
+
+#version2
+num1=int(input("enter first no: "))
+num2=int(input("enter second no: "))
+print("multiplication of two numbers is ", num1 * num2)
